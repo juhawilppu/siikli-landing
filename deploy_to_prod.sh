42078d49c534d4b33d86f0b87e710652a76e9e2d
Juha Wilppu <juha.wilppu@gmail.com>
2025-09-06 16:04:26 +0300

    refactor: rename deployment script

--- deploy_to_prod.sh
+++ deploy_to_prod.sh
@@ -2,33 +2,19 @@
 set -e
 start=$(date +%s)
 
-command -v docker >/dev/null 2>&1 || { echo >&2 "❌ Docker is not installed. Aborting."; exit 1; }
 command -v aws >/dev/null 2>&1 || { echo >&2 "❌ AWS CLI is not installed. Aborting."; exit 1; }
 
 echo "🔍 Running linter..."
 npm run lint
-npm run lint
-
-echo "🔍 Formatting prisma schema..."
-(
-    cd backend
-    npx prisma format
-)
 
 # Application version
 version=$(date +%s)
 
 echo "🏗️ Building frontend..."
-(
-    cd frontend
-    npm run build
-)
+npm run build
 
 echo "🚀 Deploying frontend..."
-(
-    cd frontend
-    aws s3 sync ./dist/ s3://siikli.fi --delete
-)
+aws s3 sync ./dist/ s3://siikli.fi --delete
 
 end=$(date +%s)
 echo "--------------------------------"
