ae83347d68b5e06f1a0a5da633d92eba2f607ef2
Juha Wilppu <juha.wilppu@gmail.com>
2025-11-01 13:21:32 +0200

    feat: enable session replay

--- src/lib/posthog.ts
+++ src/lib/posthog.ts
@@ -5,11 +5,9 @@ export function initPosthog() {
     api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
     autocapture: true,
     capture_pageview: true,
-    disable_session_recording: true,
-    debug: import.meta.env.MODE === 'development',
+    disable_session_recording: false,
     loaded: () => {
       console.log('PostHog loaded')
     },
   })
-  posthog.people.set({ variant: localStorage.getItem('variant') })
 }
